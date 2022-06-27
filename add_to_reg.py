ef720497adb2778ef5ff41206ac03602a0c439d4
MagicManatee <69170322+ArthurSudbrackIbarra@users.noreply.github.com>
2022-06-27 14:38:34 -0300

    Using single quotes for strings.

--- add_to_reg.py
+++ add_to_reg.py
@@ -17,7 +17,7 @@ icon_path = f'{cwd}\\git.ico'
 SetValueEx(outer_key, 'Icon', 0, REG_SZ, icon_path)
 
 # Creating inner key.
-inner_key = CreateKey(outer_key, "command")
+inner_key = CreateKey(outer_key, 'command')
 SetValue(inner_key, '', REG_SZ, f'{python_exe} "{cwd}\\cloner.py"')
 
-print("\n-> Command successfully added to Windows context menu!")
+print('\n-> Command successfully added to Windows context menu!')
